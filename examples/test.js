6bb05c08c5adfec0691c0727010d0e9e361bf7b8
levizwannah <levizwannah@gmail.com>
2023-06-02 20:07:45 +0300

    finished formatting

--- examples/test.js
+++ examples/test.js
@@ -7,19 +7,12 @@ jm.component("blogList", (...args) => {
     for(let blog of blogArray){
         list.push(
             jm.div(
-                {
-                    id: `div-blog-${blog.id}`,
-                    class: "blog-item-holder"
-                },
+                { id: `div-blog-${blog.id}`, class: "blog-item-holder" },
 
-                jm.h2( {id: blog.id, class: "blog-item"}, blog.text),
+                jm.h2( { id: blog.id, class: "blog-item" }, blog.text),
 
                 jm.div(
-                    {
-                        id: `div-blog-desc-${blog.id}`,
-                        class: `blog-item`
-                    },
-
+                    { id: `div-blog-desc-${blog.id}`, class: `blog-item` },
                     jm.p(`${blog.description}`)
                 )
             )
@@ -27,10 +20,7 @@ jm.component("blogList", (...args) => {
     }
 
     return jm.div(
-        {
-            id: `div-blog-holder`,
-            class: `blog-list`,
-        },
+        { id: `div-blog-holder`, class: `blog-list` },
         ...list,
         ...args.slice(1)
     );
