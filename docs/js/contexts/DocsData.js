8614686c3c969b2d43bf0b051262701f0a12cf0f
levizwannah <levizwannah@gmail.com>
2023-07-02 07:53:39 +0300

    separated data from context

--- docs/js/contexts/DocsData.js
+++ docs/js/contexts/DocsData.js
@@ -36,53 +36,9 @@ class DocsData extends OpenScript.Context {
     }
 
     installation() {
-        this.has('data').installation = {
-            icon: 'fa-tools',
-            id: 'ojs-installation',
-            title: 'Installation',
-            content: [
-                h.p(
-                    `OpenScript.Js Installation`
-                ),
-                h.p(
-                    'Section intro goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus condimentum nisl id vulputate. Praesent aliquet varius eros interdum suscipit. Donec eu purus sed nibh convallis bibendum quis vitae turpis. Duis vestibulum diam lorem, vitae dapibus nibh facilisis a. Fusce in malesuada odio.'
-                )
-            ],
-
-            sections: [
-                {
-                    id: 'ojs-installation-2',
-                    title: 'Installation 1.1',
-                    heading: 'Section Installation',
-                    content: [
-                        h.p(
-                            'Section intro goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus condimentum nisl id vulputate. Praesent aliquet varius eros interdum suscipit. Donec eu purus sed nibh convallis bibendum quis vitae turpis. Duis vestibulum diam lorem, vitae dapibus nibh facilisis a. Fusce in malesuada odio.'
-                        )
-                    ]
-                },
-                {
-                    id: 'ojs-installation-2',
-                    title: 'Installation 1.1',
-                    heading: 'Section Installation',
-                    content: [
-                        h.p(
-                            'Section intro goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus condimentum nisl id vulputate. Praesent aliquet varius eros interdum suscipit. Donec eu purus sed nibh convallis bibendum quis vitae turpis. Duis vestibulum diam lorem, vitae dapibus nibh facilisis a. Fusce in malesuada odio.'
-                        )
-                    ]
-                },
-                {
-                    id: 'ojs-installation-2',
-                    title: 'Installation 1.1',
-                    heading: 'Section Installation',
-                    content: [
-                        h.p(
-                            'Section intro goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus condimentum nisl id vulputate. Praesent aliquet varius eros interdum suscipit. Donec eu purus sed nibh convallis bibendum quis vitae turpis. Duis vestibulum diam lorem, vitae dapibus nibh facilisis a. Fusce in malesuada odio.'
-                        )
-                    ]
-                },
-
-            ]
-        }
+        autoload.req('data.Installation').then(() => {
+            this.has('data').installation = new data.Installation.Installation();
+        })
     }
 
 }
---
+++ docs/js/data/Installation.js
@@ -0,0 +1,47 @@
+class Installation {
+    icon = 'fa-tools';
+    id = 'ojs-installation';
+    title = 'Installation';
+    content = [
+        h.p(
+            `OpenScript.Js Installation`
+        ),
+        h.p(
+            'Section intro goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus condimentum nisl id vulputate. Praesent aliquet varius eros interdum suscipit. Donec eu purus sed nibh convallis bibendum quis vitae turpis. Duis vestibulum diam lorem, vitae dapibus nibh facilisis a. Fusce in malesuada odio.'
+        )
+    ];
+
+    sections = [
+        {
+            id: 'ojs-installation-2',
+            title: 'Installation 1.1',
+            heading: 'Section Installation',
+            content: [
+                h.p(
+                    'Section intro goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus condimentum nisl id vulputate. Praesent aliquet varius eros interdum suscipit. Donec eu purus sed nibh convallis bibendum quis vitae turpis. Duis vestibulum diam lorem, vitae dapibus nibh facilisis a. Fusce in malesuada odio.'
+                )
+            ]
+        },
+        {
+            id: 'ojs-installation-2',
+            title: 'Installation 1.1',
+            heading: 'Section Installation',
+            content: [
+                h.p(
+                    'Section intro goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus condimentum nisl id vulputate. Praesent aliquet varius eros interdum suscipit. Donec eu purus sed nibh convallis bibendum quis vitae turpis. Duis vestibulum diam lorem, vitae dapibus nibh facilisis a. Fusce in malesuada odio.'
+                )
+            ]
+        },
+        {
+            id: 'ojs-installation-2',
+            title: 'Installation 1.1',
+            heading: 'Section Installation',
+            content: [
+                h.p(
+                    'Section intro goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus condimentum nisl id vulputate. Praesent aliquet varius eros interdum suscipit. Donec eu purus sed nibh convallis bibendum quis vitae turpis. Duis vestibulum diam lorem, vitae dapibus nibh facilisis a. Fusce in malesuada odio.'
+                )
+            ]
+        },
+
+    ];
+}
