9ef43867c629d9ad6bb0d95b241c169c9f0781a4
levizwannah <levizwannah@gmail.com>
2023-07-25 12:32:43 +0300

    added deep object comparison for state change and async state render call behavoir

--- OpenScript.js
+++ OpenScript.js
@@ -1744,7 +1744,7 @@ var OpenScript = {
          * @param  {...any} args 
          * @returns 
          */
-        fire(...args) {
+        async fire(...args) {
             
             for(let [k, component] of this.listeners){
                 component.wrap(...args, this.signature);
@@ -1798,8 +1798,15 @@ var OpenScript = {
                     set(target, prop, value) {
                     
                         if(prop === "value") {
+                            let current = target.value;
+                            let nVal = value;
 
-                            if(target.value === value) return true;
+                            if(typeof current === "object"){
+                                current = JSON.stringify(current);
+                                nVal = JSON.stringify(nVal);
+                            }
+
+                            if(current === nVal) return true;
 
                             Reflect.set(...arguments);
 
